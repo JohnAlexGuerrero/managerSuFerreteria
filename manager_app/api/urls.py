62a0e46fbbcd16f1bf5857a2d04db88d84335c36
johnalexguerrero <johnalexguerrero@gmail.com>
2024-09-03 22:40:39 -0500

    feat(sales): add export_data_excel

---
+++ manager_app/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from api.views import list_bills, total_balance, detail_invoice, export_excel
+from main.views import list_products, filter_products
+
+urlpatterns = [
+
+    # api route
+    path('sales/list-invoices/', list_bills, name='list-invoices'),
+    path('sales/invoice/<str:number_bill>/', detail_invoice, name='detail-invoice'),
+    
+    path('sales/list-invoices/balance/', total_balance, name='list-invoices-balance'),
+    
+    path('inventory/products/',list_products,name='products'),
+    path('inventory/products/search',filter_products,name='filter_products'),
+        
+    path('export_csv/', export_excel, name='export_csv'),
+    
+]
--- manager_app/sales/urls.py
+++ manager_app/sales/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
-from sales.views import create_bill, home, list_bills, total_balance, detail_invoice
-from main.views import list_products, filter_products
+from sales.views import create_bill, home
 
 urlpatterns = [
     path('', home, name='home'),
     path('invoices/new/', create_bill, name='invoice_new'),
 
-    # api route
-    path('sales/list-invoices/', list_bills, name='list-invoices'),
-    path('sales/invoice/<str:number_bill>/', detail_invoice, name='detail-invoice'),
-    
-    path('sales/list-invoices/balance/', total_balance, name='list-invoices-balance'),
-    
-    path('inventory/products/',list_products,name='products'),
-    path('inventory/products/search',filter_products,name='filter_products'),
 ]

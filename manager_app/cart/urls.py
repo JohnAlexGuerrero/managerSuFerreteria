b251ede2175eb6ec671766ab31d4481b6a102499
john_guerrero1 <johnalexguerrero@gmail.com>
2025-06-11 21:07:16 -0500

    feat carrito de compras

--- manager_app/cart/urls.py
+++ manager_app/cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cart.views import CartView, add_item_cart
+from cart.views import CartView, add_item_cart, clean_cart
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('add/item/plus', add_item_cart, name='add_item_cart')
+    path('add/item/plus', add_item_cart, name='add_item_cart'),
+    path('clean/', clean_cart, name="clean_cart"),
 ]
